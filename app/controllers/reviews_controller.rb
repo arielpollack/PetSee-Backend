a0ccfadb64e885961bcd4942d8c584b0959323ba
yanirmta <yanir.mta@gmail.com>
2016-09-09 19:16:02 +0300

    reviews#review_about_user get the review you've written about :user_id
    review#update_existing_review
    update :review_id
    
    reviews#create
    create review for user
    
    Got to think about more input validations

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,17 +2,27 @@ class ReviewsController < ApplicationController
   before_action :authenticate
 
   # GET /reviews
+  # The reviews that other users wrote about current user
   def index
     last_index = params[:last_index] || 0
-    Rails.logger.info "Last index is: #{last_index}"
+    Rails.logger.info "user id is: #{params[:user_id]}"
     @reviews = Review.includes(:writer).where(user_id: params[:user_id]).where("id > ?", last_index).limit(20)
     @with_user = false
     @with_writer = true
   end
 
+  # The reviews that current user wrote
   def my_reviews
     last_index = params[:last_index] || 0
     @reviews = Review.includes(:user).where(writer_id: @current_user.id).where("id > ?", last_index).limit(20)
+    @with_user = true
+    @with_writer = false
+    render 'reviews/index'
+  end
+
+  # Return a review that current_user wrote on user_id
+  def review_about_user
+    @reviews = Review.includes(:user).where(writer_id: @current_user.id).where(user_id: params[:user_id])
     @with_user = true
     @with_writer = false
     render 'reviews/index'
@@ -24,25 +34,44 @@ class ReviewsController < ApplicationController
     render_error "rate must be between 1 to 5" and return if not (1..5).include? r[:rate].to_i
     render_error "feedback must contain text" and return if r[:feedback].nil? || r[:feedback] == ""
 
-    r[:user_id] = params[:user_id]
-    r[:writer_id] = @current_user.id
+    @review = Review.where(writer_id: @current_user.id, user_id: params[:user_id]).first
+    Rails.logger.info "IS NEW REVIEW? #{@review.nil?}"
 
-    review = Review.new(r)
-    @with_user = false
-    @with_writer = true
+    if @review.nil?  # New review
+      r[:user_id] = params[:user_id]
+      r[:writer_id] = @current_user.id
 
-    if review.save
-      render 'reviews/_review', :locals => {:review => review}
-    else
-      render :json => {:has_erors => true, :errors => review.errors}, :status => :unprocessable_entity
+      @review = Review.new(r)
+      @with_user = false
+      @with_writer = true
+    else  # Error - cannot create
+      render_error "You have already written a review about this user" and return
     end
+      if @review.save
+        render 'reviews/_review', :locals => {:review => @review}
+      else
+        render :json => {:has_erors => true, :errors => review.errors}, :status => :unprocessable_entity
+      end
 
   end
 
+  def update_existing_review
+    @review = Review.where(id: params[:review_id]).first
+    render_error "You are not the writer of this review" and return if not @review.writer_id == @current_user.id
+    render_error "There is no review of this number" and return if @review.nil?
+
+    @review.update(review_params)
+    if @review.save
+      render 'reviews/_review', :locals => {:review => @review}
+    else
+      render :json => {:has_erors=>"true", :errors => @review.errors, :review => @review}, :status => :unprocessable_entity
+    end
+  end
 
   private
     # Never trust parameters from the scary internet, only allow the white list through.
     def review_params
       params.require(:review).permit(:rate, :feedback, :user_id, :writer_id)
     end
+
 end
